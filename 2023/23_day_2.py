0e959ab1fdc382f07c9b71778ddb8fba2fcd0888
Matt Higgins <matt_higgins@hotmail.com>
2023-12-02 17:57:26 +0000

    Updated JSON import

--- 2023/23_day_2.py
+++ 2023/23_day_2.py
@@ -1,16 +1,6 @@
 # 23_day_2.py
 # https://adventofcode.com/2023
 
-# list_of_dict = []
-
-# with open('./2023/puzzle_input_day2.txt', 'r') as puzzle_file:
-#     puzzle_list = puzzle_file.read().split("\n")
-#     for line in puzzle_list:
-#         dict = eval(line)
-#         list_of_dict.append(dict)
-
-# print(list_of_dict)
-
 import json
 
 # Read the content from the text file
@@ -28,7 +18,11 @@ for line in content:
     # Extract individual game results by splitting based on semicolon
     results = [result.strip() for result in game_data.split(';')]
 
-    # Initialize a dictionary for the current game
+    # Initialize a dictionary for the current game. Remove the word 'Game' to get just the number
+    # and convert it to an integer. Append the results to the dictionary. Append the dictionary
+    # to the list. Repeat for each game in the file.
+    # Create an empty list to store the game results
+
     game_dict = {'Game Number': int(game_number.strip("Game")), 'Results': []}
 
     # Process each result in the game and append to the dictionary
@@ -47,6 +41,6 @@ json_data = json.dumps(game_list, indent=2)
 # Print or save the JSON data
 print(json_data[:1000])
 
-# Optionally, you can save the JSON data to a file
+# Save the JSON data to a file
 # with open('output.json', 'w') as output_file:
 #     output_file.write(json_data)
