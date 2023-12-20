b321a8ad37778c95f6baeafe924cb6dbae96d678
Matt Higgins <matt_higgins@hotmail.com>
2023-12-20 09:16:19 +0000

    Updated Day 7 Part 1

--- 2023/23_day_7.1.py
+++ 2023/23_day_7.1.py
@@ -11,56 +11,34 @@ Day 7: Camel Cards
 - One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
 - High card, where all cards' labels are distinct: 23456
 
-
 '''
 
-filename = './2023/puzzle_input_data/puzzle_input_test_day7.txt'
-# filename = './2023/puzzle_input_data/puzzle_input_day7.txt'
-
-'''
-sample data
-
-32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-QQQJA 483
-AAAKK 123
-
-'''
-
-filename = './2023/puzzle_input_data/puzzle_input_test_day7.txt'
-# filename = './2023/puzzle_input_data/puzzle_input_day7.txt'
+# filename = './2023/puzzle_input_data/puzzle_input_test_day7.txt'
+filename = './2023/puzzle_input_data/puzzle_input_day7.txt'
 
 with open(filename) as f:
     data = f.readlines()
 
 card_data = [list(line.split()) for line in data]
 
-sorted_cards_all = []
 
 card_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
                '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 
-card_combo = {
-    'high_card': [],
-    'one_pair': [],
-    'two_pair': [],
-    'three_kind': [],
-    'four_kind': [],
-    'full_house': [],
-    'five_kind': []
-}
+card_combo = {'high_card': [], 'one_pair': [], 'two_pair': [],
+              'three_kind': [], 'full_house': [], 'four_kind': [], 'five_kind': []}
 
+# Get the card combos and save in their respectives lists for sorting later
 for hand, bid in card_data:
     card_count = {card: hand.count(card) for card in hand}
 
     if 5 in card_count.values():
         card_combo['five_kind'].append((hand, int(bid)))
-    elif 3 in card_count.values() and 2 in card_count.values():
-        card_combo['full_house'].append((hand, int(bid)))
     elif 4 in card_count.values():
         card_combo['four_kind'].append((hand, int(bid)))
+    # Full House
+    elif 3 in card_count.values() and 2 in card_count.values():
+        card_combo['full_house'].append((hand, int(bid)))
     elif 3 in card_count.values():
         card_combo['three_kind'].append((hand, int(bid)))
     # Cannot count on a dict, so convert to list first
@@ -82,6 +60,8 @@ def card_winnings(sorted_cards, card_index):
         [idx * bid[1] for idx, bid in enumerate(sorted_cards, start=card_index)])
 
 
+# Sort the cards in their combos. Must pass the combos in the same order as the puzzle description to get the correct answer
+sorted_cards_all = []
 [sorted_cards_all.extend(sort_cards(hand_type, card_values))
  for hand_type in card_combo.keys()]
 
